13989eaef75561215ce70ea13b44270781df7995
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-03-01 23:19:42 -0300

    Use Costumer for Local Changes

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -8,8 +8,11 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Obtem o Produto pelo Provider
-    final Product product = Provider.of<Product>(context);
+    // Obtem o Produto pelo Provider, mas desativa a Notificação de Alterações
+    final Product product = Provider.of<Product>(
+      context,
+      listen: false,
+    );
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -33,7 +36,9 @@ class ProductItem extends StatelessWidget {
             product.name,
             textAlign: TextAlign.center,
           ),
-          leading: IconButton(
+          leading: Consumer<Product>(
+            // Insere o Provider no Icone de Favorito, já que só há alteração de estado nele
+            builder: (ctx, productItem, _) => IconButton(
               icon: Icon(
                 product.isFavorite
                     ? Icons.favorite_rounded
@@ -42,7 +47,9 @@ class ProductItem extends StatelessWidget {
                     ? Theme.of(context).colorScheme.secondary
                     : Colors.white,
               ),
-              onPressed: () => product.toggleFavorite()),
+              onPressed: () => product.toggleFavorite(),
+            ),
+          ),
           trailing: IconButton(
             onPressed: () {},
             icon: const Icon(
