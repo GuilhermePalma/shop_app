4f20ab8a240c5fec62fee8283d44b25b98aec3c1
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-03-08 13:43:51 -0300

    Customize Empty Orders

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -17,11 +17,37 @@ class OrdersPage extends StatelessWidget {
         title: const Text("Meus Pedidos"),
       ),
       drawer: CustomDrawer(namePage: Routes.routeOrders),
-      body: ListView.builder(
-        itemCount: orderProvider.itemsCount,
-        itemBuilder: (ctx, index) =>
-            OrderItem(order: orderProvider.items.elementAt(index)),
-      ),
+      body: orderProvider.itemsCount <= 0
+          ? Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Não Foi encontrado nenhum Historico de Pagamentos !",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Adicione Itens no Carrinho e Realize a Compra",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            )
+          : ListView.builder(
+              itemCount: orderProvider.itemsCount,
+              itemBuilder: (ctx, index) =>
+                  OrderItem(order: orderProvider.items.elementAt(index)),
+            ),
     );
   }
 }
