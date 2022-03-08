45c99561d623004222a828abe1395015ece6bccd
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-03-08 09:59:59 -0300

    Create Layout Details of Products

--- lib/pages/product_details_page.dart
+++ lib/pages/product_details_page.dart
@@ -6,12 +6,39 @@ class ProductDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final product = ModalRoute.of(context)!.settings.arguments as Product;
 
     return Scaffold(
       appBar: AppBar(
         title: Text(product.name),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 300,
+              child: Image.network(product.imageURL, fit: BoxFit.cover),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "R\$ ${product.price}",
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
