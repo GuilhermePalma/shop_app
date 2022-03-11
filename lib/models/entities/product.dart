28bc7c3680b4a83a31069f162e9a55524de80995
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-03-11 17:51:51 -0300

    Refactor Methods HTTP for Storage Favorites

--- lib/models/entities/product.dart
+++ lib/models/entities/product.dart
@@ -34,16 +34,18 @@ class Product with ChangeNotifier {
     notifyListeners();
   }
 
+  /// Metodo Responsavel por Alterar Controlar a Mudança de Favorito na API
   Future<void> toggleFavorite(String token, String userUID) async {
     try {
       _toggleVarFavorite();
 
-      final responseAPI = await http.put(
-        Uri.parse(
-          "${Urls.urFavoriteProducts}/$userUID/$id.json${Urls.paramAuth}$token",
-        ),
-        body: jsonEncode(isFavorite),
+      final Uri uriRequest = Uri.parse(
+        "${Urls.urFavoriteProducts}/$userUID/$id.json${Urls.paramAuth}$token",
       );
+
+      final responseAPI = isFavorite
+          ? await http.post(uriRequest, body: jsonEncode(isFavorite))
+          : await http.delete(uriRequest);
 
       // Verifica se foi bem Sucedido
       if (responseAPI.statusCode <= 400) {
