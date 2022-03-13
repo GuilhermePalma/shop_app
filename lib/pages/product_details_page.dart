afcfacdd734afc73676d24638449090e5d2e1041
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-03-13 20:02:59 -0300

    New Layout in Product Details

--- lib/pages/product_details_page.dart
+++ lib/pages/product_details_page.dart
@@ -9,39 +9,69 @@ class ProductDetailsPage extends StatelessWidget {
     final product = ModalRoute.of(context)!.settings.arguments as Product;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.name),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: 300,
-              child: Hero(
-                tag: product.id,
-                child: Image.network(product.imageURL, fit: BoxFit.cover),
+      body: CustomScrollView(
+        // slivers = Items que podem ser "scrollaveis"
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(product.name),
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Hero(
+                    tag: product.id,
+                    child: Image.network(
+                      product.imageURL,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment(0,0.8),
+                        end: Alignment(0,0),
+                        colors: [
+                          Color.fromRGBO(0, 0, 0, 0.6),
+                          Color.fromRGBO(0, 0, 0, 0),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 10),
-            Text(
-              "R\$ ${product.price}",
-              style: const TextStyle(
-                color: Colors.grey,
-                fontSize: 20,
-              ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    Text(
+                      "R\$ ${product.price}",
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 10),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      width: double.infinity,
+                      child: Text(
+                        product.description,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            const SizedBox(height: 10),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              width: double.infinity,
-              child: Text(
-                product.description,
-                textAlign: TextAlign.center,
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
