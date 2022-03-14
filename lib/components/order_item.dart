f0a3997fcf14203d04f287b7170b2ce8761e3f57
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-03-13 21:25:07 -0300

    Add Animation in Orders List

--- lib/components/order_item.dart
+++ lib/components/order_item.dart
@@ -11,56 +11,105 @@ class OrderItem extends StatefulWidget {
   State<OrderItem> createState() => _OrderItemState();
 }
 
-class _OrderItemState extends State<OrderItem> {
+class _OrderItemState extends State<OrderItem> with TickerProviderStateMixin {
   bool _isExpanded = false;
+
+  // Variaveis utilizadas no controle da Animação
+  AnimationController? _animationController;
+  Animation<double>? _opacityAnimation;
+
+  /// Inicializa o COntroller de Animações
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    _opacityAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController!,
+        curve: Curves.linear,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text("R\$ ${widget.order.total.toStringAsFixed(2)}"),
-            subtitle:
-                Text(DateFormat("dd/MM/yyyy hh:mm").format(widget.order.date)),
-            trailing: IconButton(
-              icon: Icon(
-                _isExpanded
-                    ? Icons.expand_less_rounded
-                    : Icons.expand_more_rounded,
+    // Obtem o Tamanho de Forma Fixa dos Items de uma Compra
+    final double _sizeListItem = (widget.order.products.length * 26.0) + 10;
+
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: _isExpanded ? _sizeListItem + 80 : 80,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                "R\$ ${widget.order.total.toStringAsFixed(2)}",
               ),
-              onPressed: () => setState(() => _isExpanded = !_isExpanded),
-            ),
-          ),
-          if (_isExpanded)
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: (widget.order.products.length * 26.0) + 10,
-              child: ListView(
-                children: widget.order.products.map((product) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        product.nameProduct,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        "R\$ ${product.quantityProducts}x ${product.priceItem}",
-                        style: const TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  );
-                }).toList(),
+              subtitle: Text(
+                DateFormat("dd/MM/yyyy hh:mm").format(widget.order.date),
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  _isExpanded
+                      ? Icons.expand_less_rounded
+                      : Icons.expand_more_rounded,
+                ),
+                onPressed: () => setState(() {
+                  if (_isExpanded) {
+                    _isExpanded = false;
+                    _animationController?.reverse();
+                  } else {
+                    _isExpanded = true;
+                    _animationController?.forward();
+                  }
+                }),
               ),
             ),
-        ],
+            AnimatedContainer(
+              height: _isExpanded ? _sizeListItem : 0,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.linear,
+              child: FadeTransition(
+                opacity: _opacityAnimation!,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 4,
+                  ),
+                  height: _sizeListItem,
+                  child: ListView(
+                    children: widget.order.products.map((product) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            product.nameProduct,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "R\$ ${product.quantityProducts}x ${product.priceItem}",
+                            style: const TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
